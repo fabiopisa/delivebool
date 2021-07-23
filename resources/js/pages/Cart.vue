5f8666a11bfb6a8e625dc8e583fdf275c211f31b
GianniCampana <campanagianni97@gmail.com>
2021-07-23 16:16:08 +0200

    problema background

---
+++ resources/js/pages/Cart.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+   <h1>questo è il mio carrello</h1>
+
+
+            <router-link class="btn btn-primary" :to="{name:'home'}" >
+
+                <h6>
+                    Torna alla Home
+                </h6>
+
+            </router-link>
+
+  </section>
+</template>
+
+<script>
+export default {
+    name:'Cart'
+}
+</script>
+
+<style>
+
+</style>
